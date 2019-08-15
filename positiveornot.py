b4d25f8f300062a386ffa60a06b0b91e35af6072
vignesh0794 <54101192+vignesh0794@users.noreply.github.com>
2019-08-15 11:54:42 +0530

    Add files via upload

---
+++ positiveornot.py
@@ -0,0 +1,10 @@
+num1 = float(input("Enter first number: "))
+
+ 
+if (num1 > 0):
+   print("the entered number is positive")
+elif (num1 < 0):
+   print("the entered number is negative")
+else:
+   print("The largest number is zero")
+ 
